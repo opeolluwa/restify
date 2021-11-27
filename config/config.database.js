07d7fef3bce4d598c53c7d3b2fda0996edb55719
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-11-27 14:41:10 +0100

    FIXES :: minor bug fixes

--- config/config.database.js
+++ config/config.database.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mysql = require('mysql2');
 
 
-const connection = mysql.createConnection({
+const database = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password:process.env.DB_ACCESS_KEY,
