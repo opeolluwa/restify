492c358650fe5ffd4eb97256f1af6116cb0e8cc4
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-11-29 00:41:11 +0100

    moving over to sandbox

--- templatico/index.js
+++ templatico/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const app = express()
 const cors = require('cors')
 
--- templatico/lib/utils/validator.js
+++ templatico/lib/utils/validator.js
@@ -1,3 +1,5 @@
 /* take email and validate it */
 const email_validator = require('nodejs-email-validation')
 const validate_email = (email)=> email_validator(email)
+
+module.exports = email_validator;
