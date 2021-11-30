5ae37f9a0bea140a7ced732f4f9176b96630f3b1
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-01 00:17:08 +0100

    FEAT added 2 mpre controllers 1. for getting all contacts 2. for deleting a contact

--- bin/controllers/contacts.js
+++ bin/controllers/contacts.js
@@ -1,21 +1,27 @@
 // load in dependencies
 const database = require("../config/config.database")
-const { hash_password, compare_hash } = require("../utils/bcypt") // methods from bycrypt
-const unique_id = require('../utils/uuid'); //universal user id
-const validate_email = require("../utils/validator");
-const jwt = require("../utils/jwt")
 
+
+// get all contact
+function all_contact(req, res) {
+    database.promise().query("SELECT * FROM contacts")
+        .then(([rows, fields]) => {
+            return res.send(rows);
+        })
+        .catch(error => console.log(error))
+        .then(() => database.end());
+}
 
 
 function add_contact(req, res) {
     //fetch data from pay load
     const { contact_email, contact_name, contact_phone } = req.body
-    console.log({ contact_email, contact_name, contact_phone });
 
     //check if user exists
     database.promise()
         .query("SELECT * FROM contacts WHERE LOWER(contact_email) =?", [contact_email])
         .then(([rows, fields]) => {
+
             //add user if not exist
             if (!rows[0]) {
                 database.promise().query("INSERT INTO contacts (contact_name, contact_email, contact_phone)  VALUES (?,?,?)", [contact_name, contact_email, contact_phone])
@@ -25,18 +31,45 @@ function add_contact(req, res) {
                     .catch(error => console.log(error))
                     .then(() => database.end());
             }
+
             //inform user of existence if found
             else {
                 return res.send({ message: contact_email + " already exists" })
             }
         })
-        .catch(console.log)
+        .catch(error => console.log(error))
         .then(() => database.end());
 }
 
-function remove_contact(req, res) {
 
+
+//REMOVE CONTACT
+function remove_contact(req, res) {
+    //fetch data from pay load
+    const { contact_email } = req.body
+
+    //check if user exists
+    database.promise()
+        .query("SELECT * FROM contacts WHERE LOWER(contact_email) =?", [contact_email])
+        .then(([rows, fields]) => {
+            //add user if not exist
+            if (!rows[0]) {
+                return res.status(404).send({ message: contact_email + " not found" })
+            }
+
+            //inform user of existence if found
+            else {
+                database.promise().query("DELETE * FROM contacts WHERE contact_email = ?", [contact_email])
+                    .then(([rows, fields]) => {
+                        return res.send({ message: contact_email + " successfully removed" })
+                    })
+                    .catch(error => console.log(error))
+                    .then(() => database.end());
+            }
+        })
+        .catch(error => console.log(error))
+        .then(() => database.end());
 }
 
 //export class 
-module.exports = { add_contact, remove_contact }
+module.exports = { all_contact, add_contact, remove_contact }
