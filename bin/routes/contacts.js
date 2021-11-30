c48be5ba086291208063477b9086bac648bc5b2f
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-01 00:20:13 +0100

    mapped routes to controlles 1. all contact routes work fine http://localhost:3000/contacts/all 2. remove contact rount http://localhost:3000/contacts/remove/:id not working

--- bin/routes/contacts.js
+++ bin/routes/contacts.js
@@ -1,31 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const cors = require('cors')
-const { add_contact } = require('../controllers/contacts')
+const { add_contact, remove_contact, all_contact } = require('../controllers/contacts')
 
 
 
 router.use(cors())
 //default get all contacts
-router.get('/', (req, res) => {
-    //destructure request
-    const { contact_email: email, contact_name: name, contact_phone: phone } = req.body
+router.get('/',
+)
 
-    const rx = new Contact();
-    console.log(
-        rx.email_exists(email)
-    )
-    return res.send({ email, name, phone })
-
-})
+//all contact
+router.get("/all", all_contact)
 
 //Add contact
 router.post("/add", add_contact)
-
-
-router.delete("/remove/:contact_email", (req, res) => {
-    res.send({ message: "remove contact" })
-})
+//remove contact
+router.post("/remove", remove_contact)
 
 //update contact
 router.put("/update/:contact_email", (req, res) => {
