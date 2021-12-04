c16681c12e04c0c3e52d5ec07393449ec2459cf8
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-04 05:51:05 +0100

    FEAT :: created  middleware for data validation

---
+++ bin/middleware/index.js
@@ -0,0 +1,31 @@
+const email_validation = require('nodejs-email-validation')
+//TODO:import and use express walidatorDO: 
+
+module.exports = {
+    validate_auth: (req, res, next) => {
+        //fetch data from pay load
+        const { user_email, user_first_name, user_last_name, user_phone, password } = req.body
+
+        //!use_email
+        if (!user_email || !email_validation(user_email).validate()) { return res.status(400).send({ message: "please provide a valid mail" }) }
+
+        //!user_first_name
+        //TODO: use validator
+        if (!user_first_name) { return res.status(400).send({ message: "please provide first name" }) }
+
+        //!user_last_name
+        //TODO: use validator
+        if (!user_last_name) { return res.status(400).send({ message: "please provide last name" }) }
+
+
+        //!user_last_name
+        //TODO: use validator
+        if (!user_phone) { return res.status(400).send({ message: "please provide  phone number" }) }
+
+        //!password
+        //TODO: use validator
+        if (!password || password.length < 8) { return res.status(400).send({ message: "please enter a password of minimum of  8 characters" }) }
+        
+        next();
+    }
+}
