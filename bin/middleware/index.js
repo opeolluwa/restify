135087f6db5ec34d3fa3b6d73fdd584dc8045e56
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-04 05:57:08 +0100

    UPDATE ::  added middleware to validate user login

--- bin/middleware/index.js
+++ bin/middleware/index.js
@@ -2,7 +2,7 @@ const email_validation = require('nodejs-email-validation')
 //TODO:import and use express walidatorDO: 
 
 module.exports = {
-    validate_auth: (req, res, next) => {
+    validate_auth_register: (req, res, next) => {
         //fetch data from pay load
         const { user_email, user_first_name, user_last_name, user_phone, password } = req.body
 
@@ -25,7 +25,16 @@ module.exports = {
         //!password
         //TODO: use validator
         if (!password || password.length < 8) { return res.status(400).send({ message: "please enter a password of minimum of  8 characters" }) }
-        
+
+        next();
+    },
+
+    validate_auth_login: (req, res, next) => {
+        //fetch data from pay load
+        const { user_email, user_password } = req.body
+        //!use_email
+        if (!user_email || !email_validation(user_email).validate()) { return res.status(400).send({ message: "invalid mail or password" }) }
+        if (!user_password) { return res.status(400).send({ message: "invalid email or password" }) }
         next();
     }
 }
