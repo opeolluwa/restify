e1a34046544cf076b6600672fd9e9f82d69912f6
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-04 05:57:54 +0100

    FEAT :: mounted middle ware without test

--- bin/routes/auth.js
+++ bin/routes/auth.js
@@ -2,13 +2,14 @@ const express = require('express')
 const router = express.Router()
 const cors = require('cors')
 const { register, login } = require('../controllers/auth')
-
+const { validate_auth_login, validate_auth_register } = require('../middleware')
 router.use(cors())
 
 
-router.post("/register", register) // register user
-router.post("/login", login) //login user
- //TODO: update user data
- //TODO:: reset password
+router.post("/register", validate_auth_register, register) // register user
+router.post("/login", validate_auth_login, login) //login user
+
+//TODO: update user data
+//TODO:: reset password
 
 module.exports = router
