c34077c8d3acb720a9c4d62e61524301d7b0ab6b
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-04 10:50:37 +0100

    UPDATE

--- bin/middleware/index.js
+++ bin/middleware/index.js
@@ -1,32 +1,49 @@
 const email_validation = require('nodejs-email-validation')
-//TODO:import and use express walidatorDO: 
+const iodash = require("iodash") // hellper module for string manioulation
+
+//TODO: import and use express walidatorDO: 
 
 module.exports = {
     validate_auth_register: (req, res, next) => {
         //fetch data from pay load
         const { user_email, user_first_name, user_last_name, user_phone, password } = req.body
+        const errors = []; //hold validation error 
 
         //!use_email
         //TODO: validate email with module
-        if (!user_email/*  || !email_validation(user_email).validate() */) { return res.status(400).send({ message: "please provide a valid mail" }) }
+        if (!user_email/*  || !email_validation(user_email).validate() */) {
+            errors.push(iodash.capitalize("please provide a valid mail"))
+        }
 
         //!user_first_name
         //TODO: use validator
-        if (!user_first_name) { return res.status(400).send({ message: "please provide first name" }) }
+        if (!user_first_name) {
+            errors.push(iodash.capitalize("please provide first name"))
+        }
 
         //!user_last_name
         //TODO: use validator
-        if (!user_last_name) { return res.status(400).send({ message: "please provide last name" }) }
+        if (!user_last_name) { errors.push(iodash.capitalize("please provide last name")) }
 
 
         //!user_last_name
         //TODO: use validator
-        if (!user_phone) { return res.status(400).send({ message: "please provide  phone number" }) }
+        if (!user_phone) {
+            errors.push(iodash.capitalize("please provide  phone number"))
+        }
 
         //!password
         //TODO: use validator
-        if (!password || password.length < 8) { return res.status(400).send({ message: "please enter a password of minimum of  8 characters" }) }
+        if (!password || password.length < 8) {
+            errors.push(iodash.capitalize("please enter a password of minimum of  8 characters"))
+        }
 
+        //if error send the error
+        if (errors.length) {
+            res.status(400).send(errors)
+        }
+        else return
+        
         next();
     },
 
@@ -34,8 +51,13 @@ module.exports = {
         //fetch data from pay load
         const { user_email, user_password } = req.body
         //!use_email
-        if (!user_email/*  || !email_validation(user_email).validate() */) { return res.status(400).send({ message: "invalid mail or password" }) }
-        if (!user_password) { return res.status(400).send({ message: "invalid email or password" }) }
+        if (!user_email/*  || !email_validation(user_email).validate() */) {
+            errors.push(iodash.capitalize("invalid mail or password"))
+        }
+
+        if (!user_password) {
+            errors.push(iodash.capitalize("invalid email or password"))
+        }
         next();
     }
 }
