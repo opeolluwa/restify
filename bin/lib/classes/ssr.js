e2b6c83b42878280008607ad9b6b19a70e37046f
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-09 20:41:10 +0100

    bug fixes

---
+++ bin/lib/classes/ssr.js
@@ -0,0 +1,29 @@
+const { lastIndexOf } = require("lodash");
+const database = require("../../config/config.database");
+
+var val = null
+class SSRender {
+    //takes database and object representing database fields name
+    constructor(database_name) {
+        this.database_name = database_name;
+    }
+    //api to fetch and return data from database
+    render() {
+
+        //connect to database, 
+        database.promise().query(`SELECT * FROM  ${this.database_name}`)
+            .then(([rows, fields]) => rows)
+            .then(data => { val = data; return val })
+            .catch(error => console.log(error))
+            .then(() => database.end());
+    }
+}
+module.exports = SSRender;
+
+const user = new SSRender("users")
+const render = user.render()
+//  router.get("/ssr", render)
+
+//  module.exports = router
+
+console.log(render)
