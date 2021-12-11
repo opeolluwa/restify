d09b9fe93a8ffe02a8d6e3133f989f817b8ac377
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-11 08:50:09 +0100

    FEAT :: added fetching fields to ssr 2. Bug :: couldn't return database val from caller

--- bin/lib/classes/ssr.js
+++ bin/lib/classes/ssr.js
@@ -1,32 +1,34 @@
-const { lastIndexOf } = require("lodash");
-const database = require("../../config/config.database");
+const database = require("../../config/config.database"); // load in db util
 
-var val = null
 class SSRender {
-    //takes database and object representing database fields name
-    constructor(database_name) {
-        this.database_name = database_name;
+    //takes database name and  an Array representing database fields to be fetched
+    constructor(database_name, ...fields) {
+        this.database_name = database_name; //get database name
+        this.fields = fields // get  fields
     }
-    //api to fetch and return data from database
-    render() {
 
-        //connect to database, 
-        database.promise().query(`SELECT * FROM  ${this.database_name}`)
+    //api to fetch and return data from database using provided database name
+    render() {
+        //placeholder for  number of fields to get , 
+        const place_holder = Array.from("?".repeat(this.fields.length)).join(",") // return  (?, ? ... , ?) 
+
+        //prepare query to  conditionally fetch all data if non field is provided or data from fields provided.
+        /*  const query = (this.fields === null) ? "SELECT * FROM " + this.database_name : `SELECT ${place_holder} FROM ${this.database_name}` */
+
+        const query = `SELECT ${place_holder} FROM ${this.database_name}`
+
+        //connect to database,  fetch data
+        database.promise().query(query, this.fields)
             .then(([rows, fields]) => rows)
-            .then(data => { val = data;})
+            .then(data => { console.log(data); })
             .catch(error => console.log(error))
             .then(() => database.end());
 
-            console.log(val)
     }
-    
+
 }
 module.exports = SSRender;
 
-const user = new SSRender("users")
+const user = new SSRender("users", "user_id", "user_first_name", "user_email", "password")
 const render = user.render()
-//  router.get("/ssr", render)
-
-//  module.exports = router
-
 console.log(render)
