d45367eed5586d4e708bf57f2fbf449c6dcf3643
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-11 09:13:37 +0100

    BUG FIXES :: fixed database returnuing values such as [ {user_id : user_id, .... } Bug due to passing literals as second argument to database instead of variable  => [user_id] instead of [user_id]

--- bin/lib/classes/ssr.js
+++ bin/lib/classes/ssr.js
@@ -9,26 +9,19 @@ class SSRender {
 
     //api to fetch and return data from database using provided database name
     render() {
-        //placeholder for  number of fields to get , 
-        const place_holder = Array.from("?".repeat(this.fields.length)).join(",") // return  (?, ? ... , ?) 
-
-        //prepare query to  conditionally fetch all data if non field is provided or data from fields provided.
-        /*  const query = (this.fields === null) ? "SELECT * FROM " + this.database_name : `SELECT ${place_holder} FROM ${this.database_name}` */
-
-        const query = `SELECT ${place_holder} FROM ${this.database_name}`
-
+        //prepare query to get provided fields or all if no field is provided
+        const query = `SELECT ${this.fields.join(",") || "*"} FROM ${this.database_name}`
         //connect to database,  fetch data
-        database.promise().query(query, this.fields)
+        database.promise().query(query)
             .then(([rows, fields]) => rows)
             .then(data => { console.log(data); })
             .catch(error => console.log(error))
             .then(() => database.end());
-
     }
 
 }
 module.exports = SSRender;
 
-const user = new SSRender("users", "user_id", "user_first_name", "user_email", "password")
+const user = new SSRender("users")
 const render = user.render()
 console.log(render)
