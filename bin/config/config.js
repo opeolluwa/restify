1f23f6e26793ea4ccb0865ee163c15b35e07fd99
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-12 04:24:42 +0100

    FEAT migrated to env variables

---
+++ bin/config/config.js
@@ -0,0 +1,27 @@
+
+//load in dependencies
+require("dotenv").config()
+
+module.exports = {
+  "development": {
+    "username": "sillicone",
+    "password": null,
+    "database": "restify",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
