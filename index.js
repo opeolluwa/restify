087ce3e98f90fa38bd5a1aa721a6b0560d5b2031
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-14 16:27:50 +0100

    REFACTOR

--- index.js
+++ index.js
@@ -1,47 +1,9 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000
-
-const Sequelize = require("sequelize")
-
-const sequelize = new Sequelize("restify", "sillicone", "", {
-    host: "localhost",
-    dialect: "mysql"
-})
-
-const Student = sequelize.define("student", {
-    firstName: {
-        type: Sequelize.STRING,
-    },
-    lastName: {
-        type: Sequelize.STRING,
-    },
-}, {
-    freezeTableName: true
-
-})
-
-Student.sync({ force: true })
-    .then(() => {
-        return Student.create({
-            firstName: "opeoluwa",
-            lastName: "adeoye adefemi"
-        })
-    }).catch((error) => {
-        console.log("AN ERROR OCCURRED------------------------" + error)
-    })
-
-// cass User.
-
-// database.sequelize.sync().then((req) => {
-
-// }).catch((error) => {
-//     console.log(error.message);
-// })
-
-app.listen(PORT, () => {
-    console.log("ignition started on port:" + PORT);
-})
+//load in dependencies
+const { sequelize } = require("./models");
 
 
+async function main() {
+    await sequelize.sync()
+}
 
+main()
--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
@@ -32,6 +32,6 @@ Object.keys(db).forEach(modelName => {
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// db.Sequelize = Sequelize; dont export sequelize from here
 
 module.exports = db;
