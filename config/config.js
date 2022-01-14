cb99d891e7388a5215e2bde3a3abc85d004274fb
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-15 00:11:56 +0100

    REFACTOR :: using env variable

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+module.exports = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_SCHEMA,
+    "host": process.env.BD_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
