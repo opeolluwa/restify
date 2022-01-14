83ceb918171f2cb43bf65aecc14036f7b26c6c04
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-15 00:16:54 +0100

    FEAT :: created basic field for users'

--- models/user.js
+++ models/user.js
@@ -14,9 +14,72 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
+    /*
+           * store user id as primary key
+          * generate key with UUID
+          * uuid must be unique and not null
+          */
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      unique: true
+    },
+
+
+    /*
+    * store user firstname
+    * name must not be null
+    */
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+
+
+    /*
+    * store user lastname
+    * name must not be null
+    */
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+
+    /*
+    * store user email,
+    * email must not be null
+    * email must be unique 
+    * email must not be nul
+    */
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isEmail: true, //validate email
+      validate: {
+        notEmpty: true
+      }
+    },
+
+    /*
+    * store user phone with country code,
+    * phone may be null
+    * phone must be unique 
+    */
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
   }, {
     sequelize,
     modelName: 'User',
