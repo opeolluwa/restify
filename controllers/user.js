ccf94f9072f9e039a15d77e802e2bb1103960ded
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-15 01:31:30 +0100

    FEAT :: added sequelize-bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,13 @@
 //load in dependencies
-const {  User } = require("./../models")
+const { User } = require("./../models")
+
 
 async function register(req, res) {
     //get fields from payload
-    const { firstname, lastname, email, phone } = req.body
+    const { firstname, lastname, email, phone, password } = req.body
     //try to create user
     try {
-        const user = await User.create({ firstname, lastname, email, phone })
+        const user = await User.create({ firstname, lastname, email, phone, password })
         return res.send({ user })
     } catch (error) {
         res.send({ error })
