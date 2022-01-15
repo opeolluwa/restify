983cc93a522ef096b34ce11f0d68cb8893b9d73c
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-15 01:32:40 +0100

    UPDATE

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 //load in dependencies
+const { sequelize, User } = require("./models");
+
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
 //import routes
 const __users = require("./routes/user")
-app.use("/users",__users)
+app.use("/users", __users)
+
+
 app.listen(PORT, async () => {
+    await sequelize.sync()
     console.log("ignition started on port:" + PORT);
 })
