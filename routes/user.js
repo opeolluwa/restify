aa78e915bc6263930eede508533b7923b05cc3f7
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-21 01:53:17 +0100

    CHORE :: added login endpoint BUG :: couldnt generate token to send if password is right

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { register } = require('../controllers/user');
+const { register, login } = require('../controllers/user');
 const router = express.Router();
 
 router.post("/register", register)
-
+router.post("/login", login)
 module.exports = router;
