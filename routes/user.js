ef43e96702ff0f27330c63f7d7843edcc9edc89a
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-24 23:30:44 +0100

    1. CHORE :: added profile end point 2. BUG :: couldn't get user data from database by validating jwt in header

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const { register, login } = require('../controllers/user');
+const { register, login, profile } = require('../controllers/user');
+const { validate_auth_token, decode_jwt } = require("./../middleware")
 const router = express.Router();
 
 router.post("/register", register)
 router.post("/login", login)
+
+//send in user detail, validate token from login or refresh first
+router.get("/profile", validate_auth_token, decode_jwt, profile)
+
 module.exports = router;
