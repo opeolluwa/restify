28b2578e2677928f6b01358fe0119517f43ac412
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-24 23:32:00 +0100

    CHORE added profile end point BUG :: error fetching user data using jwt

--- controllers/user.js
+++ controllers/user.js
@@ -8,13 +8,26 @@ const jwt = require("jsonwebtoken")
 async function register(req, res) {
     //get fields from payload
     const { firstname, lastname, email, phone, password } = req.body
-    //try to create user
+
+    //check if user already exists using predefined primary key, email, username, or  phone
+    const user = await User.findOne({ where: { email } })
+    // const user = await User.findOne({ where: { phone } })
+    // const user = await User.findOne({ where: { username } })
+
+    //if user is found 
+    if (user) {
+        //return field by which the user is identified and a  message stating user already exists
+        const user_id = user.email || user.phone || user.username;
+        return res.send({ error: true, message: ` ${user_id} already exists` })
+    }
+
+    //try to create user  if not found
     try {
         const user = await User.create({ firstname, lastname, email, phone, password })
-        //TODO; check if user already exists
-        return res.send({ error: null, message: user.email + " successfully added" })
+        return res.send({ error: null, message: `${user.email}  successfully added` })
     } catch (error) {
-        res.send({ error })
+        // TODO: add generic error message
+        return res.send({ error })
     }
 }
 
@@ -43,10 +56,8 @@ async function login(req, res) {
     //if the password is valid send token send if not  error
     if (validPassword) {
         // generate and send token back to request 
-
-        //TODO: remove user password  from token  payload 
-        const token = jwt.sign({ user }, process.env.JWT_KEY, { expiresIn: '1h' });
-        res.send({ token })
+        const token = jwt.sign({ email }, process.env.JWT_KEY, { expiresIn: '1h' });
+        return res.send({ token })
 
     }
     else {
@@ -56,7 +67,20 @@ async function login(req, res) {
 
 
 //user profile information
-async function profile(req, res) {
-    //TODO: build user profile end point 
+ function profile(req, res) {
+    /*get user object from decode_jwt middleware,
+      * the fields returned are
+      * {firstname, lastname, email}
+      * get all required fields and then send them back to user
+      */
+    const { user } = req.user;
+    const email = user.email
+
+    //get user data from database 
+
+    // const profile = await User.findOne({ where: { email: user.email } })
+    // const user = await User.findOne({ where: { phone } })
+    // const user = await User.findOne({ where: { username } })
+    return res.send(user)
 }
 module.exports = { register, login, profile }
