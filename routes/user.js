146ae5ed3b9e7be538c67f1384ebe6645cb9ac34
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-26 02:22:43 +0100

    CHORE :: added update endpoint for handling account update

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const { register, login, profile } = require('../controllers/user');
+const { register, login, profile, update} = require('../controllers/user');
 const { validate_auth_token, decode_jwt } = require("./../middleware")
 const router = express.Router();
 
+//register user
 router.post("/register", register)
+
+//login user, send token if success
 router.post("/login", login)
 
 //send in user detail, validate token from login or refresh first
 router.get("/profile", validate_auth_token, decode_jwt, profile)
 
+//update user profile, validate token from login or refresh first
+router.post("/update", validate_auth_token, decode_jwt, update)
 module.exports = router;
