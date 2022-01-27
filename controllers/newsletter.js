34e8de4d18c382ad6ebbc2c97bdebf4dec9df5a5
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-27 02:21:52 +0100

    CHORE :: init newsletter controllers

---
+++ controllers/newsletter.js
@@ -0,0 +1,19 @@
+//load in dependencies
+const { NewsLetter } = require("./../models")
+require("dotenv").config()
+
+
+async function subscribe(req, res) {
+
+}
+
+
+async function unsubscribe(req, res) {
+
+}
+
+
+async function confirmSubscription(req, res) {
+    
+}
+module.exports = { subscribe, unsubscribe, confirmSubscription }
