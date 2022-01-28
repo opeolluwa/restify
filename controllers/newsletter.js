c6b09c47418740ae09d95092755606ba89d54059
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-28 23:33:21 +0100

    CHORE :: created subscription controller half way, waiting to integrate node mailer

--- controllers/newsletter.js
+++ controllers/newsletter.js
@@ -1,9 +1,53 @@
 //load in dependencies
-const { NewsLetter } = require("./../models")
 require("dotenv").config()
+const { NewsLetter } = require("./../models") //database model
+const nodemailer = require('nodemailer'); //package to handle mails
+const otpGenerator = require('otp-generator'); //package to generate one time password (OTP)
+const jwt = require("jsonwebtoken")
 
 
-async function subscribe(req, res) {
+//controller to add subscriber to newsletter list
+async function subscribe(req, res, next) {
+    //accept subscriber email, check if mail is valid then send token to the mail box for validation
+    const { email } = req.body
+
+    if (!email) {
+        return res.send({ message: "Invalid email", error: true })
+    }
+    try {
+        //check if subscriber already exist
+        const subscriber = await NewsLetter.findOne({ where: { email } });
+
+        //if subscriber is found 
+        if (subscriber) {
+            //return field by which the subscriber is identified and a  message stating subscriber already exists
+            const subscriber_id = subscriber.email;
+            return res.send({ error: true, message: ` ${subscriber_id} already exists` })
+        }
+    } catch (error) {
+        return res.send({ error: "An internal error occurred, please retry after some time." })
+        // return res.send({ error })
+
+    }
+
+    //try to create subscriber if it does not exist
+    try {
+        //generate 12 length token
+        let otp = otpGenerator.generate(12, { upperCaseAlphabets: true, specialChars: true });
+        const subscriber = await NewsLetter.create({ email, registrationToken: otp });
+
+        // generate and send token back to request 
+        const token = jwt.sign({ email, otp }, process.env.JWT_KEY, { expiresIn: '24h' });
+       
+        //TODO: send token as payload to node mailer
+
+        return res.send({ error: null, verify, token, message: `A token was sent to ${subscriber.email}, please input the token to complete the subscription` })
+    } catch (error) {
+        // add generic error message on failed request
+        return res.send({ error: "An internal error occurred, please retry after some time." })
+        // return res.send({ error })
+
+    }
 
 }
 
@@ -12,8 +56,8 @@ async function unsubscribe(req, res) {
 
 }
 
-
+//confirm the mail added is valid then send a token to validate the registration
 async function confirmSubscription(req, res) {
-    
+
 }
 module.exports = { subscribe, unsubscribe, confirmSubscription }
